9c376f15137279ba0e4abe5ec912dc8245559f59
AnaseVe <87339737+AnaseVe@users.noreply.github.com>
2022-03-22 15:52:56 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+
+}
+
+function setup(){
+canvas = createCanvas(300,300);
+canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save('https://tse2.mm.bing.net/th?id=OIP._qQAoSxNApsgXOUmLt1X7QHaHa&pid=Api&P=0&w=154&h=154');
+
+}
